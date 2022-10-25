28cedcb32f9100bbdd5af2bce587617c8e57140b
flavio.santana <9090flavio@gmail.com>
2022-10-24 21:16:47 -0300

    feat: mapeando uma entidade

---
+++ src/main/java/io/com/store/jpa/entity/Produto.java
@@ -0,0 +1,58 @@
+package io.com.store.jpa.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "desc_nome")
+    private String nome;
+
+    @Column(name = "desc_descricao")
+    private String descricao;
+
+    @Column(name = "vlr_preco")
+    private BigDecimal valor;
+
+    public Produto() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
